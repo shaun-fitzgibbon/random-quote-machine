46947049feefdff182efd29c26ed242b9005f4d7
Shaun Fitzgibbon <shaun@fitzindustries.com>
2023-04-03 21:16:26 +0200

    add loading spinner

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,32 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import Script from "next/script";
 
 import "@/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
+      <Head>
+        <title>{`🦾 Random quote machine`}</title>
+        <meta
+          name="description"
+          content="fetches random famous quotes from the internet"
+        />
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+          crossOrigin="anonymous"
+        /> */}
+      </Head>
+      <Script
+        async
+        defer
+        src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"
+      />
       <Component {...pageProps} />
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import LoadingSpinner from "@/components/loading-spinner";
 import { type NextPage } from "next";
 import Head from "next/head";
+import { title } from "process";
 import { useEffect, useState } from "react";
 import { FaQuoteLeft, FaTwitter } from "react-icons/fa";
 
@@ -27,70 +29,69 @@ const Home: NextPage = () => {
     fetchQuote();
   }, []);
 
-  if (!quote) return <p>No Quote to display</p>;
+  if (!quote || quote === null) return <p>No Quote to display</p>;
 
   return (
-    <>
-      <Head>
-        <title>Random quote machine</title>
-        <meta
-          name="description"
-          content="fetches random famous quotes from the internet"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="max-sm: container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white">
-            Random <span className="text-[hsl(280,100%,70%)]">Quote</span>{" "}
-            Machine
-          </h1>
-          <div
-            id="quote-box"
-            className="h-full w-[640px] max-w-screen-sm rounded-md bg-[#2e026d]/80 p-11 text-white"
-          >
-            {!isLoading ? (
-              <figure>
-                <blockquote className="flex flex-row gap-3">
-                  <i className="top-0 text-5xl leading-3">
-                    <FaQuoteLeft />
-                  </i>
-                  <p id="text" className="text-3xl">
-                    {quote.quote}
-                  </p>
-                </blockquote>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <div className="max-sm: container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <h1 className="text-5xl font-extrabold tracking-tight text-white">
+          Random <span className="text-[hsl(280,100%,70%)]">Quote</span> Machine
+        </h1>
+        <div
+          id="quote-box"
+          className="h-full w-[640px] max-w-screen-sm rounded-md bg-[#2e026d]/80 p-11 text-white"
+        >
+          {!isLoading ? (
+            <figure>
+              <blockquote className="flex flex-row gap-3">
+                <i className="top-0 text-5xl leading-3 text-[hsl(280,100%,70%)]">
+                  <FaQuoteLeft />
+                </i>
+                <p id="text" className="text-3xl">
+                  {quote.quote}
+                </p>
+              </blockquote>
 
-                <figcaption className="p-2 text-right font-thin">
-                  - <span id="author">{quote.author}</span>
-                </figcaption>
-              </figure>
-            ) : (
-              <div>Loading....</div>
-            )}
+              <figcaption className="p-2 text-right font-thin">
+                - <span id="author">{quote.author}</span>
+              </figcaption>
+            </figure>
+          ) : (
+            <div className="flex flex-col items-center justify-center p-5">
+              <LoadingSpinner size={40} />
+            </div>
+          )}
 
-            <div className="flex flex-row justify-between">
-              <a
-                id="tweet-quote"
-                href={`https://twitter.com/intent/tweet?text="${quote.quote}" - ${quote.author}`}
-                className="flex items-center justify-center rounded-sm bg-[hsl(280,100%,70%)] px-3 py-2 align-middle text-white"
-                target="_blank"
+          <div className="flex flex-row justify-between">
+            <a
+              id="tweet-quote"
+              href={`https://twitter.com/intent/tweet?text="${quote.quote}" - ${quote.author}`}
+              className="flex items-center justify-center rounded-sm bg-[hsl(280,100%,70%)] px-3 py-2 align-middle text-white hover:bg-[hsl(280,73%,72%)]"
+              target="_blank"
+            >
+              <FaTwitter />
+            </a>
+            <div>
+              <button
+                id="new-quote"
+                className="flex flex-row rounded-sm bg-[hsl(280,100%,70%)] px-3 py-2 text-white hover:bg-[hsl(280,73%,72%)]"
+                onClick={() => fetchQuote()}
+                disabled={isLoading}
               >
-                <FaTwitter />
-              </a>
-              <div>
-                <button
-                  id="new-quote"
-                  className="rounded-sm bg-[hsl(280,100%,70%)] px-3 py-2 text-white"
-                  onClick={() => fetchQuote()}
-                >
-                  Get Quote
-                </button>
-              </div>
+                {!isLoading ? (
+                  <p>Get Quote</p>
+                ) : (
+                  <div className="flex flex-row items-center justify-center">
+                    <LoadingSpinner color="fill-white" />
+                    <p>Get Quote</p>
+                  </div>
+                )}
+              </button>
             </div>
           </div>
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 };
 
